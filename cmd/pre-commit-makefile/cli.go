8f9656a890b11a6b909ac54ebbdde34fe4da2d83
Vadim Gedz <vadims@linux-tech.io>
2023-10-18 21:24:51 +0300

    feat: add version flag

--- cmd/pre-commit-makefile/cli.go
+++ cmd/pre-commit-makefile/cli.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 )
 
+const version = "0.1.5"
+
 func cli() error {
 	var readmePath string
 	var sectionName string
+	var printVersion bool
 
 	var cmdRun = &cobra.Command{
 		Use:   "run",
@@ -28,7 +34,17 @@ func cli() error {
 	cmdRun.Flags().StringVarP(&readmePath, "readme-path", "r", "README.md", "Path to the readme file")
 	cmdRun.Flags().StringVarP(&sectionName, "section-name", "s", "## Makefile targets", "Readme section name to put the description in")
 
-	var rootCmd = &cobra.Command{Use: "pre-commit-makefile"}
+	var rootCmd = &cobra.Command{
+		Use: "pre-commit-makefile",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if printVersion {
+				fmt.Println(version)
+				os.Exit(0)
+			}
+		},
+	}
+
+	rootCmd.PersistentFlags().BoolVarP(&printVersion, "version", "v", false, "Print the version and exit")
 	rootCmd.AddCommand(cmdRun)
 
 	if err := rootCmd.Execute(); err != nil {
