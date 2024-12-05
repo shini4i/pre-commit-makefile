e30b9878ab739adca3fb684bc127bd3f0464afe7
Vadim Gedz <vadims@linux-tech.io>
2024-12-05 18:27:18 +0200

    Bump version: 0.2.1 → 0.2.2

--- cmd/pre-commit-makefile/cli.go
+++ cmd/pre-commit-makefile/cli.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.2.1"
+const version = "0.2.2"
 
 func runCommand(readmePath *string, sectionName *string) *cobra.Command {
 	return &cobra.Command{
